4719f70641107ec21d9aa861564bc6cc3d4a5d69
milanranisavljevic <32481249+milanranisavljevic@users.noreply.github.com>
2017-10-26 02:29:29 +0200

    Add files via upload

--- LocationTracker/app/src/main/java/com/lt/milanranisavljevic/locationtracker/NewTrack.java
+++ LocationTracker/app/src/main/java/com/lt/milanranisavljevic/locationtracker/NewTrack.java
@@ -1,29 +1,42 @@
 package com.lt.milanranisavljevic.locationtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class NewTrack extends AppCompatActivity {
+public class NewTrack extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_track);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        init();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    private void init() {
+        EditText name = (EditText)findViewById(R.id.etName);
+        EditText description = (EditText)findViewById(R.id.etDescription);
+        Button btnStart = (Button)findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(this);
+        if( name.getText().toString().length() == 0)
+            name.setError( "Name is required!" );
+        if( description.getText().toString().length() == 0)
+            description.setError( "Description is required!" );
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnStart:
+                Intent intentNewTrack = new Intent(this, TrackDetails.class);
+                startActivity(intentNewTrack);
+        }
+    }
 }
