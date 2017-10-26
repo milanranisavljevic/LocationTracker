d6a42478893577a284ca1ee1146c12807352f646
milanranisavljevic <32481249+milanranisavljevic@users.noreply.github.com>
2017-10-26 02:27:59 +0200

    Add files via upload

---
+++ LocationTracker/app/src/main/java/com/lt/milanranisavljevic/locationtracker/TrackDetails.java
@@ -0,0 +1,20 @@
+package com.lt.milanranisavljevic.locationtracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class TrackDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.track_details);
+    }
+
+
+}
